7ae38104f100b6d7aa19c9c28dcc05f93ccf2f3d
ALEKSAGK <aleksamileticgk@gmail.com>
2020-09-27 20:17:46 +0200

    Dodata baza

--- korisnici.sql
+++ korisnici.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 24, 2020 at 05:51 PM
--- Server version: 10.4.13-MariaDB
--- PHP Version: 7.4.8
+-- Generation Time: Sep 27, 2020 at 08:16 PM
+-- Server version: 10.4.14-MariaDB
+-- PHP Version: 7.4.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -29,10 +29,18 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `korisnici` (
   `ID` int(11) NOT NULL,
-  `Ime` varchar(50) NOT NULL,
+  `Ime` varchar(255) NOT NULL,
   `Email` varchar(255) NOT NULL,
   `Poruka` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `korisnici`
+--
+
+INSERT INTO `korisnici` (`ID`, `Ime`, `Email`, `Poruka`) VALUES
+(1, 'Aleksa', 'aleksa@hotmail.com', '25.9.2020.'),
+(2, 'Aleksa', 'aleksa@hotmail.com', 'Želim da poručim 3l piva na svoje ime.');
 
 --
 -- Indexes for dumped tables
@@ -52,7 +60,7 @@ ALTER TABLE `korisnici`
 -- AUTO_INCREMENT for table `korisnici`
 --
 ALTER TABLE `korisnici`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
